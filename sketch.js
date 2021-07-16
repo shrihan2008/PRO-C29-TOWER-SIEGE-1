0ba14f1e5305b3ec1f5cec75fabd2cbb7bd342f1
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2021-07-16 21:11:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,11 +3,10 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Constraint = Matter.Constraint;
-var  polygon_img
-function preload(){
-   polygon_img=loadImage("polygon.png")
-   } 
-
+var img
+function prload(){
+  img=loaddImage("polygon.png")
+}
 function setup (){
   
     createCanvas(800, 400);
@@ -16,9 +15,6 @@ function setup (){
 
 
     Engine.run(engine);
-
-
-
 
 
   
@@ -34,17 +30,13 @@ function setup (){
     tower7 = new Tower(550, 150, 30, 100);
     tower8 = new Tower(610, 150, 30, 100);
     tower9 = new Tower(580, 50, 30, 100);
-
-    ball=new Polygon(50,200,20)
-    ball.addImage=(polygon_img)
-    ball.resize=(50,50)
- 
-  
-
-   
+    polygon=new Polygon(50,200,20)
+    
+    
+    sling = new Sling(polygon.body,{x:50,y:50});
 
   }
- 
+
 
 function draw() {
   background(76);  
@@ -58,15 +50,16 @@ function draw() {
  tower7.display()
  tower8.display()
  tower9.display()
- ground.display()
- ball.display()
+  ground.display()
+  polygon.display()
+ sling.display()
   
 }
-//function mouseDragged(){
-//  Matter.Body.setPosition(sling.body, {x: mouseX , y: mouseY});
-//}
+function mouseDragged(){
+  Matter.Body.setPosition(polygon.body, {x: mouseX , y: mouseY});
+}
 
 
-//function mouseReleased(){
-//  sling.fly();
-//}
+function mouseReleased(){
+  sling.fly();
+}
